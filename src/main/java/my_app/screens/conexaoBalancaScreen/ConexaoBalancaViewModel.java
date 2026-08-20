8601dc8ab35cfba7a551397ce1df6c13b763ae1b
camilleDGBTecnologia <camilledgbtecnologia@gmail.com>
2026-08-20 16:33:31 -0300

    mostra erro ao tentar carregar balanças

--- src/main/java/my_app/screens/conexaoBalancaScreen/ConexaoBalancaViewModel.java
+++ src/main/java/my_app/screens/conexaoBalancaScreen/ConexaoBalancaViewModel.java
@@ -56,21 +56,22 @@ public class ConexaoBalancaViewModel {
     public void load() {
         Async.Run(() -> {
             try {
-//                String[] portNames = SerialPortList.getPortNames();
-//                UI.runOnUi(() -> {
-//                    for (String name : portNames) portasComState.add(name);
-//                });
+                String[] portNames = SerialPortList.getPortNames();
+                UI.runOnUi(() -> {
+                    for (String name : portNames) portasComState.add(name);
+                });
 
-                SerialPort[] ports = SerialPort.getCommPorts();
-                System.out.println("Portas encontradas: " + ports.length);
-                for (SerialPort port : ports) {
-                    String name = port.getSystemPortName() + " - " + port.getDescriptivePortName();
-                    System.out.println(name);
-
-
-                }
+//                SerialPort[] ports = SerialPort.getCommPorts();
+//                System.out.println("Portas encontradas: " + ports.length);
+//                for (SerialPort port : ports) {
+//                    String name = port.getSystemPortName() + " - " + port.getDescriptivePortName();
+//                    System.out.println(name);
+//
+//
+//                }
             } catch (Throwable e) {
                 log.error("Erro ao carregar portas seriais: {}", e.getMessage(), e);
+                UI.runOnUi(() -> Components.ShowAlertError("Erro ao carregar portas seriais: " + e.getMessage()));
             }
 
             try {
